799d0785eb240ec12879fdc50f0931491e244483
Alessandro-Moroni <alemoro98@gmail.com>
2024-02-20 16:02:21 +0100

    [finish]all

--- js/script.js
+++ js/script.js
@@ -4,17 +4,19 @@ const eta = prompt('Quanti anni hai?');
 
 const costoChilometri = 0.21;
 
-let costoBiglietto = chilometri * costoChilometri;
+let costoBiglietto = (chilometri * costoChilometri).toFixed(2);
 
+
+
+if(eta < 18) {
+  costoBiglietto = ((costoBiglietto / 100) * 80);
+}
+if(eta > 65) {
+  costoBiglietto = ((costoBiglietto / 100) * 60);
+}
 
 document.getElementById('user').innerHTML = `
 <p>
 Il costo del tuo biglietto è di ${costoBiglietto}€ e percorrerai ${chilometri} km.
 </p>
 `;
-
-const scontoMinori = 20;
-
-if(eta < 18) {
-  costoBiglietto = (costoBiglietto / 20) ;
-}
